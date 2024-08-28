c5a4af71e5c84038fb76569388ff29f62b0c60c1
johnpgr <jp.greidinger@hotmail.com>
2024-08-28 19:36:52 -0300

    maybe not

--- apps/auth-proxy/routes/r/[...auth].ts
+++ apps/auth-proxy/routes/r/[...auth].ts
@@ -2,11 +2,9 @@ import { Auth } from "@auth/core"
 import GoogleProvider from "@auth/core/providers/google"
 import { eventHandler, toWebRequest } from "h3"
 
-export const runtime = "edge"
-
 export default eventHandler(async (event) =>
     Auth(toWebRequest(event), {
-        basePath: "/",
+        basePath: "/r",
         secret: process.env.AUTH_SECRET,
         trustHost: !!process.env.VERCEL,
         redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
