545d5bb2ce814200887196e77f170cd68ca703ed
johnpgr <jp.greidinger@hotmail.com>
2024-08-29 08:49:06 -0300

    log

--- apps/auth-proxy/middleware/forward-callback.ts
+++ apps/auth-proxy/middleware/forward-callback.ts
@@ -2,7 +2,7 @@ import {eventHandler, getRequestHeaders, readBody } from "h3"
 
 export default eventHandler(async (event) => {
     if(event.node.req.url?.startsWith("/callback/google")){
-        console.log("Forwarding request")
+        console.log("Forwarding request", event.node.req)
         const forwardedUrl = "/r/" + event.node.req.url
         const forwardedReq = new Request(forwardedUrl, {
             method: event.node.req.method,
