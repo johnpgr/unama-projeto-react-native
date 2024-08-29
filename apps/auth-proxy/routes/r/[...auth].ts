a62df76872c04074c253847ab88d2576185b580c
johnpgr <jp.greidinger@hotmail.com>
2024-08-29 09:47:08 -0300

    log

--- apps/auth-proxy/routes/r/[...auth].ts
+++ apps/auth-proxy/routes/r/[...auth].ts
@@ -2,17 +2,20 @@ import { Auth } from "@auth/core"
 import GoogleProvider from "@auth/core/providers/google"
 import { defineEventHandler, toWebRequest } from "h3"
 
-export default defineEventHandler(async (event) =>
-    Auth(toWebRequest(event), {
-        basePath: "/r",
-        secret: process.env.AUTH_SECRET,
-        trustHost: !!process.env.VERCEL,
-        redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
-        providers: [
-            GoogleProvider({
-                clientId: process.env.AUTH_GOOGLE_ID,
-                clientSecret: process.env.AUTH_GOOGLE_SECRET,
-            }),
-        ],
-    }),
+export default defineEventHandler(
+    async (event) => (
+        console.log(event.node.req),
+        Auth(toWebRequest(event), {
+            basePath: "/r",
+            secret: process.env.AUTH_SECRET,
+            trustHost: !!process.env.VERCEL,
+            redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
+            providers: [
+                GoogleProvider({
+                    clientId: process.env.AUTH_GOOGLE_ID,
+                    clientSecret: process.env.AUTH_GOOGLE_SECRET,
+                }),
+            ],
+        })
+    ),
 )
