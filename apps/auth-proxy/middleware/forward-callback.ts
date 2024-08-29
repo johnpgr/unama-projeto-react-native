fa30858a1f9782432fb6641e371cd293f9e7b856
johnpgr <jp.greidinger@hotmail.com>
2024-08-29 08:38:07 -0300

    forward maybe?

---
+++ apps/auth-proxy/middleware/forward-callback.ts
@@ -0,0 +1,16 @@
+import {eventHandler, getRequestHeaders, readBody } from "h3"
+
+export default eventHandler(async (event) => {
+    if(event.node.req.url?.startsWith("/callback/google")){
+        console.log("Forwarding request")
+        const forwardedUrl = "/r/" + event.node.req.url
+        const forwardedReq = new Request(forwardedUrl, {
+            method: event.node.req.method,
+            //@ts-expect-error ok
+            headers: getRequestHeaders(event),
+            body: await readBody(event),
+        })
+        //@ts-expect-error ok
+        return await $fetch(forwardedReq)
+    }
+})
--- apps/auth-proxy/routes/callback/google.get.ts
+++
@@ -1,6 +0,0 @@
-import {eventHandler} from "h3"
-
-export default eventHandler((event) => {
-    console.log(event.node.req.originalUrl)
-    return "Hello"
-})
