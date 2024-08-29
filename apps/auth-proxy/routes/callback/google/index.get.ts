82a724927cd99db30e7dfde7d54253683eceaeee
johnpgr <jp.greidinger@hotmail.com>
2024-08-29 09:55:16 -0300

    wtf

--- apps/auth-proxy/routes/callback/google/index.get.ts
+++ apps/auth-proxy/routes/callback/google/index.get.ts
@@ -4,8 +4,8 @@ export default defineEventHandler(async (event) => {
     const url = "/r" + event.node.req.url
     const headers = getRequestHeaders(event)
     console.log({ url, headers })
-    return await fetch(url, {
-        //@ts-expect-error ok
+    //@ts-expect-error ok
+    return await $fetch(url, {
         headers,
     })
 })
