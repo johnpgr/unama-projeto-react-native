d3c9e766ff9d725b884ef4734b26ec06a552c348
johnpgr <jp.greidinger@hotmail.com>
2024-08-29 09:44:07 -0300

    last try

--- apps/auth-proxy/routes/callback/google/index.get.ts
+++ apps/auth-proxy/routes/callback/google/index.get.ts
@@ -1,15 +1,11 @@
-import { defineEventHandler, getRequestHeaders, readBody } from "h3"
+import { defineEventHandler, getRequestHeaders } from "h3"
 
 export default defineEventHandler(async (event) => {
     const url = "/r" + event.node.req.url
-    const method = event.method
     const headers = getRequestHeaders(event)
-    const body = await readBody(event)
-    console.log({ url, method, headers, body })
+    console.log({ url, headers })
     //@ts-expect-error ok
     return await $fetch(url, {
-        method,
         headers,
-        body,
     })
 })
