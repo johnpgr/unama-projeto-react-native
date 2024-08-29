7db2cdd0ee0be1db8295f68d0158f7c6a0c22e7d
johnpgr <jp.greidinger@hotmail.com>
2024-08-29 09:36:57 -0300

    i have hope3

--- apps/auth-proxy/routes/callback/google/[...forward].ts
+++ apps/auth-proxy/routes/callback/google/[...forward].ts
@@ -2,11 +2,13 @@ import { defineEventHandler, getRequestHeaders, readBody } from "h3"
 
 export default defineEventHandler(async (event) => {
     const url = "/r" + event.node.req.url
+    const method = event.method
     const headers = getRequestHeaders(event)
     const body = await readBody(event)
-    console.log({ url, headers, body })
+    console.log({ url, method, headers, body })
     //@ts-expect-error ok
     return await $fetch(url, {
+        method,
         headers,
         body,
     })
