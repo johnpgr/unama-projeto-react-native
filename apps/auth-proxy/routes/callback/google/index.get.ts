c0403f763c591a1f0b6e2feb5ca430b1ff10bd0d
johnpgr <jp.greidinger@hotmail.com>
2024-08-29 09:21:57 -0300

    i have hope2

--- apps/auth-proxy/routes/callback/google/index.get.ts
+++ apps/auth-proxy/routes/callback/google/index.get.ts
@@ -1,10 +1,8 @@
-import { defineEventHandler, getRequestHeaders, readBody } from "h3"
+import { defineEventHandler, getRequestHeaders } from "h3"
 
 export default defineEventHandler(async (event) => {
     //@ts-expect-error ok
     return await $fetch("/r" + event.node.req.url, {
-        method: event.node.req.method,
         headers: getRequestHeaders(event),
-        body: await readBody(event),
     })
 })
