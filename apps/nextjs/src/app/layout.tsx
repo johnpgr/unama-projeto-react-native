230d8219f718d4c5b990862805ad213889f6df8b
johnpgr <jp.greidinger@hotmail.com>
2024-09-04 16:32:17 -0300

    remove default apps view from template

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,7 +1,4 @@
 import type { Metadata, Viewport } from "next"
-import { GeistMono } from "geist/font/mono"
-import { GeistSans } from "geist/font/sans"
-
 import { cn } from "@projeto/web-ui"
 import { ThemeProvider, ThemeToggle } from "@projeto/web-ui/theme"
 import { Toaster } from "@projeto/web-ui/toast"
@@ -10,28 +7,8 @@ import { TRPCReactProvider } from "~/trpc/react"
 
 import "~/app/globals.css"
 
-import { env } from "~/env"
-
 export const metadata: Metadata = {
-    metadataBase: new URL(
-        env.VERCEL_ENV === "production"
-            ? "https://turbo.t3.gg"
-            : "http://localhost:3000",
-    ),
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    openGraph: {
-        title: "Create T3 Turbo",
-        description:
-            "Simple monorepo with shared backend for web & mobile apps",
-        url: "https://create-t3-turbo.vercel.app",
-        siteName: "Create T3 Turbo",
-    },
-    twitter: {
-        card: "summary_large_image",
-        site: "@jullerino",
-        creator: "@jullerino",
-    },
+    title: "Hello, World",
 }
 
 export const viewport: Viewport = {
@@ -44,13 +21,7 @@ export const viewport: Viewport = {
 export default function RootLayout(props: { children: React.ReactNode }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body
-                className={cn(
-                    "min-h-screen bg-background font-sans text-foreground antialiased",
-                    GeistSans.variable,
-                    GeistMono.variable,
-                )}
-            >
+            <body className="min-h-screen bg-background font-sans text-foreground antialiased">
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
