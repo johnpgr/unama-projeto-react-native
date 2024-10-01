1b063f546a394a47b80abda04ba1f86d6927b8db
johnpgr <jp.greidinger@hotmail.com>
2024-09-30 21:03:45 -0300

    adjust max-width for oauth signin buttons

--- apps/expo/src/components/buttons-signin-oauth.tsx
+++ apps/expo/src/components/buttons-signin-oauth.tsx
@@ -6,11 +6,11 @@ import { GoogleSignInButton } from "./signin-google"
 export function SigninOAuthButtons() {
     return (
         <View className="flex flex-row gap-2 py-4">
-            <Pressable className="w-1/2 border rounded-2xl border-border/50 px-4 py-2 flex flex-row items-center justify-center gap-2">
+            <Pressable className="w-[49%] border rounded-2xl border-border/50 px-4 py-2 flex flex-row items-center justify-center gap-2">
                 <Text className="text-border">Entrar com</Text>
                 <GoogleSignInButton />
             </Pressable>
-            <Pressable className="w-1/2 border rounded-2xl border-border/50 px-4 py-2 flex flex-row items-center justify-center gap-2">
+            <Pressable className="w-[49%] border rounded-2xl border-border/50 px-4 py-2 flex flex-row items-center justify-center gap-2">
                 <Text className="text-border">Entrar com</Text>
                 <GithubSignInButton />
             </Pressable>
