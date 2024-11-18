aca8efb3b10e5d9fc2f326cbcfeb0fba29d9b064
johnpgr <jp.greidinger@hotmail.com>
2024-11-18 19:38:47 -0300

    add some styles to received-points userExtract list

--- expo/src/app/(tabs)/points/index.tsx
+++ expo/src/app/(tabs)/points/index.tsx
@@ -39,11 +39,12 @@ export default function PointsPage() {
   const { user } = useAuth()
 
   const { data: userExtract } = api.transaction.getUserExtract.useQuery()
+  const keys = Object.keys(userExtract ?? {})
 
   if (!user) return null
 
   return (
-    <View className="flex-1 bg-gray-100">
+    <View className="flex-1 bg-white">
       <View className="items-center bg-emerald-700 p-4">
         <Text className="text-lg text-white">Pontuação atual</Text>
         <Text className="text-6xl font-bold text-white">
@@ -71,29 +72,40 @@ export default function PointsPage() {
           </TouchableOpacity>
         </Link>
       </View>
-      <View>
-        <Text> Histórico </Text>
-        <Text>{JSON.stringify(userExtract, null, 2)}</Text>
+      <View className="flex flex-col gap-4">
+        <Text className="px-4 text-lg font-medium">Histórico</Text>
+        {userExtract ? (
+          <FlatList
+            data={keys}
+            keyExtractor={(date) => date}
+            renderItem={({ item }) => {
+              const value = userExtract[item]
+              if (!value) return null
+              return (
+                <View>
+                  <Text className="px-2 py-1 text-lg font-medium text-gray-700">
+                    {item}
+                  </Text>
+                  <FlatList
+                    data={value}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                      <NotificationItemView notification={item} />
+                    )}
+                    contentContainerStyle={{ paddingBottom: 20 }}
+                  />
+                </View>
+              )
+            }}
+          />
+        ) : (
+          <View className="p-4">
+            <Text className="text-center text-gray-100">
+              Nehuma notificação encontrada
+            </Text>
+          </View>
+        )}
       </View>
-
-      {/* <View className="mt-4 flex-1 border-t border-border"> */}
-      {/*   {userExtract.length === 0 ? ( */}
-      {/*     <View className="p-4"> */}
-      {/*       <Text className="text-center text-gray-100"> */}
-      {/*         Nehuma notificação encontrada */}
-      {/*       </Text> */}
-      {/*     </View> */}
-      {/*   ) : ( */}
-      {/*     <FlatList */}
-      {/*       data={userExtract} */}
-      {/*       keyExtractor={(item) => item.id} */}
-      {/*       renderItem={({ item }) => ( */}
-      {/*         <NotificationItemView notification={item} /> */}
-      {/*       )} */}
-      {/*       contentContainerStyle={{ paddingBottom: 20 }} */}
-      {/*     /> */}
-      {/*   )} */}
-      {/* </View> */}
     </View>
   )
 }
@@ -101,20 +113,56 @@ export default function PointsPage() {
 export function NotificationItemView({
   notification,
 }: {
-  notification: Notification
+  notification: NonNullable<
+    RouterOutputs["transaction"]["getUserExtract"][string]
+  >[number]
 }) {
-  return (
-    <View className="flex-row justify-between border-b border-gray-200 p-4">
-      <View>
-        <Text>{notification.points} pontos</Text>
-        <Text className="text-base">{notification.type}</Text>
-        <Text className="text-gray-500">
-          Data: {formatDatePTBR(notification.transactionDate)}
-        </Text>
-      </View>
-      <TouchableOpacity>
-        <Text className="text-emerald-700">→</Text>
-      </TouchableOpacity>
-    </View>
-  )
+  switch (notification.type) {
+    case "p2pFrom":
+    case "p2pTo":
+      return (
+        <View className="flex-row justify-between border-b border-gray-200 p-4">
+          <View>
+            <Text>{notification.points} pontos</Text>
+            <Text className="text-base">{notification.type}</Text>
+            <Text className="text-gray-500">
+              Data: {formatDatePTBR(notification.createdAt)}
+            </Text>
+          </View>
+          <TouchableOpacity>
+            <Text className="text-emerald-700">→</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    case "reward":
+      return (
+        <View className="flex-row justify-between border-b border-gray-200 p-4">
+          <View>
+            <Text>Recompensa: {notification.rewardId}</Text>
+            <Text className="text-base">{notification.type}</Text>
+            <Text className="text-gray-500">
+              Data: {formatDatePTBR(notification.createdAt)}
+            </Text>
+          </View>
+          <TouchableOpacity>
+            <Text className="text-emerald-700">→</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    case "recycling":
+      return (
+        <View className="flex-row justify-between border-b border-gray-200 p-4">
+          <View>
+            <Text>Reciclagem: {notification.weight}kg</Text>
+            <Text className="text-base">{notification.type}</Text>
+            <Text className="text-gray-500">
+              Data: {formatDatePTBR(notification.createdAt)}
+            </Text>
+          </View>
+          <TouchableOpacity>
+            <Text className="text-emerald-700">→</Text>
+          </TouchableOpacity>
+        </View>
+      )
+  }
 }
--- expo/src/app/(tabs)/scan/received-points.tsx
+++ expo/src/app/(tabs)/scan/received-points.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Image, Pressable, Text, View } from "react-native"
-import { useLocalSearchParams } from "expo-router"
+import { Link, useLocalSearchParams } from "expo-router"
 import { z } from "zod"
 
 export const ReceivedPointsParams = z.object({
@@ -16,26 +16,26 @@ export default function ReceivedPointsScreen() {
   const params = useLocalSearchParams()
   const parsedParams = ReceivedPointsParams.safeParse(params)
   if (!parsedParams.success) return null
-  const { points, sender } = parsedParams.data
+  const { points } = parsedParams.data
 
   return (
-    <View className="mt-4 flex flex-1 flex-col px-4">
-      <View>
+    <View className="flex flex-1 flex-col items-center gap-4 bg-background px-4 pt-4">
+      <View className="flex flex-col items-center">
         <Image source={require("../../../../assets/checkmark.png")} />
-        <Text>Parabéns! Você recebeu {points} pontos</Text>
+        <Text className="text-lg font-medium">
+          Parabéns! Você recebeu {points} pontos
+        </Text>
       </View>
       <View className="h-[1px] w-full bg-border"></View>
-      <View>
-        <Text>Detalhes da ação:</Text>
-        <Text className="font-bold">DETALHES</Text>
-        <View>
-          <Pressable>
-            <Text>Ver detalhes</Text>
-          </Pressable>
-          <Pressable>
+      <View className="flex flex-row items-center gap-4">
+        <Pressable className="flex min-w-40 flex-row justify-center rounded-md bg-gray-300 p-4 active:bg-gray-300/90">
+          <Text>Ver detalhes</Text>
+        </Pressable>
+        <Link asChild href="/(tabs)/points">
+          <Pressable className="flex min-w-40 flex-row justify-center rounded-md bg-gray-300 p-4 active:bg-gray-300/90">
             <Text>Ver meus pontos</Text>
           </Pressable>
-        </View>
+        </Link>
       </View>
     </View>
   )
